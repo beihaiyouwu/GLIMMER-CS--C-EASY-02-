1052333f944bc3b3a4f6bbe86c54c385ae68c63c
the best <259451938@qq.com>
2026-09-12 14:31:06 +0800

    函数练习

--- first.c
+++ first.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    unsigned num,num2;
-    num=0;
-    num2=num-1;
-    printf("输出%u,%u\n.",num,num2);
-    
-   
-    return 0;
+    int old,day;
+    scanf("%d",&day);
+    old=day*365;
+   printf("%d,%d.\n",old,day);
 }
