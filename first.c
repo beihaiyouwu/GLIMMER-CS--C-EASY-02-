b4a6613d4358377168ea83beb38237994c5549e8
the best <259451938@qq.com>
2026-09-12 21:13:51 +0800

    好累

--- first.c
+++ first.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include<string.h>
  int main(void)
-{
-    float num;
-    printf("Enter a floating-point value:\n");
-    scanf("%f",&num);
-    printf("%f\n",num);
-    printf("%e\n",num);
-    printf("%a",num);
+{   int num1,num2;
+    char name1[40],name2[40];
+    printf("输入姓名:");
+    scanf("%s%s",name1,name2);
+    num1=strlen(name1);
+    num2=strlen(name2);
+    printf("%s %s\n%*d %*d\n",name1,name2,num1,num1,num2,num2);
+    printf("%s %s\n%d %*d",name1,name2,num1,num1,num2);
+
     return 0;
    
 }
