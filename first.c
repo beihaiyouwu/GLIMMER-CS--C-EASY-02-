aaf7557a9a6bb885dfce803da523ac08a95ff198
the best <259451938@qq.com>
2026-09-18 18:57:13 +0800

    while循环

--- first.c
+++ first.c
@@ -1,19 +1,12 @@
-#include <stdio.h>
-#define hour 60
- int main(void)
-{  
-    int num1,second,num;
-    printf("输入时间(分钟)；");
-    scanf("%d",&num1);
-    while(num1>0)
-    {
-    num=num1/hour;
-    second=num1%hour;
-     printf("%d时%d分\n",num,second);
-     printf("请再次输入；");
-     scanf("%d",&num1);
-    }
-    return 0;
 
-   
+#include <stdio.h>
+int main(void)
+{  
+  int i=1,sum=0;
+  while(i<=10)
+  {
+    sum+=i;
+    i++;
+  }
+  printf("sum=%d",sum);
 }
