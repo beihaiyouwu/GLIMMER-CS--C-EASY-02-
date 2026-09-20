6a1f75226fa35373a11609bef61b51307d3c6b49
beihaiyouwu <259451938@qq.com>
2026-09-20 16:16:58 +0800

    Add files via upload

--- swap.c
+++ swap.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
-void swap(int *m,int *n);
+#include<stdio.h>\\通过传地址的方式实现ab值的交换，在地址上实现值的更改。
+void swap(int *m,int *n);\\声明函数，形参是指针
 int main()
 {
   int a=10,b=20;
   printf("a=%d b=%d\n",a,b);
-  swap(&a,&b);
+  swap(&a,&b);\\存ab地址
   printf("now a=%d b=%d\n",a,b );
   return 0;
 }
-void swap(int *m,int *n)
+void swap(int *m,int *n)\\定义函数
 {
     int s;
-    s=*m;
+    s=*m;  \\指针解引并赋值给中间量s
     *m=*n;
     *n=s;
 }
